93e530628215ec69759fa652e9e5bbc88ca1678f
EsperanzaHerrera <espe_herrera@hotmail.com>
2018-12-07 12:37:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/guillermorios/ingresoautos/model/Persona.java
@@ -0,0 +1,59 @@
+package com.example.guillermorios.ingresoautos.model;
+
+public class Persona {
+
+    private String IDP;
+    private String Nombre;
+    private String Apellido;
+    private String Correo;
+    private  String Contraseña;
+
+    public Persona() {
+
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
+
+    public String getIDP() {
+        return IDP;
+    }
+
+    public void setIDP(String IDP) {
+        this.IDP = IDP;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return Correo;
+    }
+}
